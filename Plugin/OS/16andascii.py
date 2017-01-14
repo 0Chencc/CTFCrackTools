b19208c4a51dfb670b74693fc3beb059a4aebcc1
A先森 <admin@hi-ourlife.com>
2017-01-15 02:03:32 +0800

    上传16进制和字符串互换....一处注释问题

--- Plugin/OS/16andascii.py
+++ Plugin/OS/16andascii.py
@@ -1,12 +1,4 @@
 #coding=utf-8
-'''
-{
-    author:A先森
-    title:字符串 16进制 互相转换
-    detail:经过一名网友提示，故此编写这个插件。也是给一个插件的例子
-    type:crypto
-}
-'''
 import binascii
 def ascii216(string):
     return binascii.b2a_hex(string)
